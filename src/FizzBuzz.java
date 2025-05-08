99dccafbf1552141b2127e10822409c3c3ba8ece
joshuazcchen <joshuaz.chen@mail.utoronto.ca>
2025-05-08 19:14:44 -0400

    did stuff

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,21 +8,23 @@ class FizzBuzz {
         int k = 100;
         int i = 1;
         while (i < k) {
-            i = fizzbuzzinging(i);
+            i = fizzbuzzinging(i, k);
         }
     }
 
-    private static int fizzbuzzinging(int i) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            System.out.println("Fizz Buzz");
-        } else if (i % 3 == 0) {
-            System.out.println("Fizz");
-        } else if (i % 5 == 0) {
-            System.out.println("Buzz");
-        } else {
-            System.out.println(i);
+    static int fizzbuzzinging(int i, int k) {
+        while (i < k) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            i++;
         }
-        i++;
         return i;
     }
 }
